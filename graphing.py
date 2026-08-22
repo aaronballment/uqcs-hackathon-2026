8d6f8706ec0786e5581f5bee91ae159b44abeb8e
Nirvan Panesar <nirvanpanesar@12-26-98-c6-a3-b1.walkup.net.uq.edu.au>
2026-08-22 18:55:23 +1000

    changes made to graphing - made prettier

--- graphing.py
+++ graphing.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 #from main import extract_math
 
 
-def latex_conversion(latex: str, filename: str,  x_min, x_max) -> bool:
+def latex_conversion(latex: str, filename: str, ) -> bool:
     expression = parse_latex(latex)
     x = sp.Symbol("x")
     
@@ -15,16 +15,37 @@ def latex_conversion(latex: str, filename: str,  x_min, x_max) -> bool:
     x_values = np.linspace(x_min, x_max, 400)
     y_values = func(x_values)
 
-
     fig, ax = plt.subplots(figsize=(6, 6))
 
-    ax.plot(x_values, y_values, color='#00ffcc', linewidth=3.5)
+    '''
+    Attempting to implement style changes to make the graph more readable
+        set_title()
+        <xlabel, ylabel>
+
+        plot(..., color=#1A365D, linewidth=, linestyle=)
+    '''
+
+    ax.plot(x_values, y_values, color="#1051AB", linewidth=4)
+
+    for spine in ['top', 'right' ]:
+        ax.spines[spine].set_visible(False)
+
+    ax.spines['bottom'].set_linewidth(4.5)
+    ax.spines['bottom'].set_color("#D51E1E")
+    ax.spines['left'].set_linewidth(4.5)
+    ax.spines['left'].set_color("#D51E1E")
+
+    ax.grid(axis='y', linestyle = '-', linewidth = 1.5, color = '#E2E8F0', zorder = 0)
+    ax.grid(axis = 'x', linestyle = '-', linewidth = 1.5, color = '#E2E8F0', zorder = 0)
+    ax.set_axisbelow(True)
+
+    plt.tick_params('both', labelcolor = "#D51E1E", labelsize = 14)
 
     output_dir = "plots"
     os.makedirs(output_dir, exist_ok=True)
     filepath = os.path.join(output_dir, filename)
 
-    plt.savefig(filepath, transparent = True)
+    plt.savefig(filepath, transparent=True)
     plt.close()
 
     return True
@@ -33,3 +54,4 @@ def latex_conversion(latex: str, filename: str,  x_min, x_max) -> bool:
 if __name__ == "__main__":
     latex_conversion(r"x^2 - 4", "test_plot2.png")
     print("SUCCESS")
+    
